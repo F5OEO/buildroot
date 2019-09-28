f50923b9fa110a8e5eb4dd7475f242ae6c0e5da0
F5OEO <evaristec@gmail.com>
2019-09-28 22:23:59 +0200

    Add personal path, Fixme shoudl be adapt for each environment

---
+++ setenv_F5OEO.sh
@@ -0,0 +1,6 @@
+#source me
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=$PATH:/opt/Xilinx/SDK/2018.3/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin
+export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2018.3/settings64.sh
+export BR2_EXTERNAL=/home/linuxdev/prog/pluto/plutosdr-fw/buildroot/f5oeo
+export LDFLAGS='--sysroot=/home/linuxdev/prog/pluto/plutosdr-fw/buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/'
